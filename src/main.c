0d7c3d97fc9576b06464feb5e825fc47deb6ec44
0xA1M <Ai.Mokrane@proton.me>
2024-07-25 12:03:51 +0100

    test

--- src/main.c
+++ src/main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-typedef unsigned int uint;
-
 int main(int argc, char **argv) {
   if (argc < 2) {
     printf("Usage: %s url\n", *argv);
     return 1;
   }
 
-  for (uint i = 1; i < argc; i++) {
+  for (int i = 1; i < argc; i++) {
     printf("%s\n", argv[i]);
   }
 
